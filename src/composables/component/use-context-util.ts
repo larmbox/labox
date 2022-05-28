741720794b9d24646f4a0ada7ad023122edb0448
Jonatan Wackström <29772763+jonataw@users.noreply.github.com>
2022-05-28 20:58:09 +0000

    Add comments to util composables

--- src/composables/component/use-context-util.ts
+++ src/composables/component/use-context-util.ts
@@ -8,28 +8,54 @@ export function useContextUtil(
 ) {
   const slots = useSlots();
 
-  function first<R>(...args: R[]): R | undefined {
+  /**
+   * Returns the first non-null or non-undefined argument.
+   *
+   * @param args
+   */
+  const first = <R>(...args: R[]): R | undefined => {
     return args.find(
       (arg) => !!arg || typeof arg === 'boolean' || typeof arg === 'number'
     );
-  }
+  };
 
-  function className(c: string): string {
+  /**
+   * Returns name prefixed with the global class prefix.
+   *
+   * @param name
+   */
+  const className = (name: string): string => {
     const { config } = _useLabox();
-    return `${config.value.classPrefix}${c}`;
-  }
+    return `${config.value.classPrefix}${name}`;
+  };
 
-  function classComponentName(name2: string | undefined): string {
+  /**
+   * Returns name prefixed with the global class prefix + component name.
+   *
+   * @param name
+   */
+  const classComponentName = (name: string | undefined): string => {
     if (!component)
       throw new Error('Cannot use classComponentName without instance.');
-    if (!name2) return '';
-    return className(`${component.name}-${name2}`);
-  }
+    if (!name) return '';
+    return className(`${component.name}-${name}`);
+  };
 
-  function hasSlot(slotName: string): boolean {
-    return !!slots[slotName];
-  }
+  /**
+   * Returns true if the component has been provided a slot with name.
+   *
+   * @param name Name of slot to search for.
+   */
+  const hasSlot = (name: string): boolean => {
+    return !!slots[name];
+  };
 
+  /**
+   * Same as classComponentName, but accepts an object with multiple names.
+   * @see classComponentName
+   *
+   * @param obj
+   */
   function classNameList(
     obj: Record<string, boolean | undefined>
   ): Record<string, boolean> {
--- src/composables/component/use-util.ts
+++ src/composables/component/use-util.ts
@@ -1,14 +1,20 @@
 export function useUtil() {
-  const isServer = () => {
-    return typeof window === 'undefined';
-  };
-
+  /**
+   * Returns true if running on client.
+   */
   const isClient = () => {
     return typeof window !== 'undefined';
   };
 
+  /**
+   * Returns true if running on server.
+   */
+  const isServer = () => {
+    return typeof window === 'undefined';
+  };
+
   return {
-    isServer,
     isClient,
+    isServer,
   };
 }
